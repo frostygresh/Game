9cb737ea9b5468e987c55de41ab3bec9ba982f4c
Brian Frost <brianfrost@consolidated.net>
2018-04-11 22:03:34 -0500

    Initial sync

---
+++ Game/Cardgame.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Cardgame.h"
+#include <iostream>
+
+using namespace std;
+
+int Cardgame::totalParticipants = 0;
+
+Cardgame::Cardgame(int players)
+	: players(players)
+{
+	totalParticipants += players;
+	cout << players << " players have started a new game. There are now "
+		<< totalParticipants << " players in total." << endl;
+}
+
+Cardgame::~Cardgame()
+{
+	totalParticipants -= players;
+	cout << players << " players have finished their game. There are now "
+		<< totalParticipants << " players in total." << endl;
+}
